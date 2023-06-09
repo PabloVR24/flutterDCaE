dfc2e3306e4950f8f15b86c43b98562c2be5ff30
PabloVR24 <pavara_2010@hotmail.com>
2023-06-09 14:09:10 -0600

    Termino de la Seccion 9

---
+++ 03_flutter_init/widgets_app/widgets_app/lib/presentation/screens/animated/animated_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AnimatedScreed extends StatelessWidget {
+  static const name = 'animated_screen';
+  const AnimatedScreed({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ 03_flutter_init/widgets_app/widgets_app/lib/presentation/screens/infinite_screoll/infinite_scroll_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class InfititeScroll extends StatelessWidget {
+  static const name = 'infinite_screen';
+  const InfititeScroll({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
