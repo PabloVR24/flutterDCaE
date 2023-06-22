b79140b1fb3a58bcd04f41bd62416696055d8c1a
PabloVR24 <pavara_2010@hotmail.com>
2023-06-22 11:33:45 -0600

    Inicio Seccion 26

---
+++ 03_flutter_init/teslo-app/lib/features/shared/widgets/custom_filled_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomFilledButton extends StatelessWidget {
+  final void Function()? onPressed;
+  final String text;
+  final Color? buttonColor;
+
+  const CustomFilledButton(
+      {super.key, this.onPressed, required this.text, this.buttonColor});
+
+  @override
+  Widget build(BuildContext context) {
+    const radius = Radius.circular(10);
+
+    return FilledButton(
+        style: FilledButton.styleFrom(
+          backgroundColor: buttonColor,
+        ),
+        onPressed: onPressed,
+        child: Text(text));
+  }
+}
